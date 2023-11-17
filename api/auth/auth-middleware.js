cdb9107f801c64ca516a1f60cd53a1282f851075
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-17 15:03:12 -0500

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,23 @@
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../../config/index')
+
 // AUTHENTICATION
 const restricted = (req, res, next) => {
-  next()
+  const token = req.headers.authorization
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (error, decoded) => {
+      if (error) {
+        next({status: 401, message: `token bad:${error.message} `})
+      }
+      else {
+        req.decodedJwt = decoded
+        next()
+      }
+    })
+  }
+  else {
+    next({status:401, message: 'what? no token?'})
+  }
 }
 
 // AUTHORIZATION
